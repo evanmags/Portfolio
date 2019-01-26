579ea4ba9ec3950b0c626305134d3e3b5814d0cc
emags112 <magnussen.evan@gmail.com>
2019-01-26 00:11:09 -0500

    set up email service

--- routes/index.js
+++ routes/index.js
@@ -14,9 +14,16 @@ router.get('/', function(req, res){
 })
 
 router.post('/mail', function(req, res){
+    transporter.verify(function(error, success) {
+        if (error) {
+          console.log(error);
+        } else {
+          console.log("Server is ready to take our messages");
+        }
+      });
     let mailOptions = {
         from: `${req.body.email.Fname} ${req.body.email.Lname} <${req.body.email.email}>`, // sender address
-        to: process.env.GMAILUSER, // list of receivers
+        to: "magnussen.evan@gmail.com", // list of receivers
         subject: "New message from portfolio", // Subject line
         text: `${req.body.email.body}, Contact ${req.body.email.Fname}: ${req.body.email.phone}, ${req.body.email.business}. `, // plain text body
       };
